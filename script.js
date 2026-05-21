e69fbadad520b264ef0762be2d07b5e9b3378c4e
Sushant Saykad <69181604+SushantSaykad@users.noreply.github.com>
2026-05-21 14:54:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener('click', function () {
+    const select = document.getElementById('colorSelect');
+    select.remove(select.selectedIndex);
+});
